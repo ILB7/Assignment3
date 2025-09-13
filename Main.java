ee278b1002ae2de8ff97f00ad4728f05b5dc8e15
ILB7 <89233518+ILB7@users.noreply.github.com>
2025-09-13 16:52:25 -0400

    test 3

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
 
-  System.out.println("Hello World");
+  public static void main(String [] args){
+
+     System.out.println("Hello World");
   System.out.println("Pizza");
+
+  }
+
 }
