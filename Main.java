07e9b9a6c43730a49696c3ece2c6975782dc93f5
Emmycodes22 <ebuko16@gmail.com>
2025-09-17 21:12:42 -0400

    Edited

--- Main.java
+++ Main.java
@@ -1,11 +1,4 @@
-public class Main {
-
-  public static void main(String [] args){
-
-     System.out.println("Hello World");
-  System.out.println("Pizza")
-  System.out.println("Sushi");
-
-  }
-
-}
+/* I just added the Book file to read the records from the csv file, and then the book Demo 
+file puts them into a object and prints them out. I added the task11 to display everything
+in the assignment up to question 11. The task11Loader is what helps run the code so I need 
+both of the files. */
