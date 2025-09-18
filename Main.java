bafb7d158a60cbbc32ee18716fe178b21aa51da6
Emmycodes22 <ebuko16@gmail.com>
2025-09-17 21:15:35 -0400

    More Comments

--- Main.java
+++ Main.java
@@ -2,3 +2,9 @@
 file puts them into a object and prints them out. I added the task11 to display everything
 in the assignment up to question 11. The task11Loader is what helps run the code so I need 
 both of the files. */
+
+/* the code needs to be ran in the terminal so to run the task 11 file I ran the following 
+in the terminal, javac Task11.java Book.java Task11Loader.java
+then java Task11.
+ */
+ */
